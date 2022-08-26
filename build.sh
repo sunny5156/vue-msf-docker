ccb2c8fbf93124dcf0798f92561fecd777ac47b3
sunny5156 <sunny5156@qq.com>
2022-08-26 11:27:46 +0800

    edit

--- build.sh
+++ build.sh
@@ -12,4 +12,5 @@ BASE_IMAGE_VERSION=$2
 echo "$BASE_IMAGE_PROJECT $BASE_IMAGE_VERSION" 
 
 # sudo docker build -t dockerhub.suntekcorps.com:8443/suntek/vue-msf-docker-swoole-4.x:$VERSION --build-arg version=$VERSION .
-sudo docker build -t dockerhub.suntekcorps.com:8443/suntek/$BASE_IMAGE_PROJECT:$BASE_IMAGE_VERSION --build-arg base_image_project=$BASE_IMAGE_PROJECT --build-arg  base_image_version=$BASE_IMAGE_VERSION .
+sudo docker build --squash -t dockerhub.suntekcorps.com:8443/suntek/$BASE_IMAGE_PROJECT:$BASE_IMAGE_VERSION --build-arg base_image_project=$BASE_IMAGE_PROJECT --build-arg  base_image_version=$BASE_IMAGE_VERSION .
+#sudo docker build  -t dockerhub.suntekcorps.com:8443/suntek/$BASE_IMAGE_PROJECT:$BASE_IMAGE_VERSION --build-arg base_image_project=$BASE_IMAGE_PROJECT --build-arg  base_image_version=$BASE_IMAGE_VERSION .
