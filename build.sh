ca13e3fd436fd189be9bbd4b17120270704c2b77
sunny5156 <sunny5156@qq.com>
2023-05-10 17:43:58 +0800

    squash特性移出,使用copy　--from　代替　Dockerfile-GRPC

--- build.sh
+++ build.sh
@@ -11,7 +11,32 @@ BASE_IMAGE_VERSION=$2
 
 echo "$BASE_IMAGE_PROJECT $BASE_IMAGE_VERSION" 
 
+#原始镜像
+ORIGIN_TAG=$BASE_IMAGE_PROJECT:$BASE_IMAGE_VERSION
+
+#压缩镜像
+SQUASH_TAG=dockerhub.suntekcorps.com:8443/suntek/$BASE_IMAGE_PROJECT:$BASE_IMAGE_VERSION
+
 # sudo docker build -t dockerhub.suntekcorps.com:8443/suntek/vue-msf-docker-swoole-4.x:$VERSION --build-arg version=$VERSION .
 #sudo docker build --squash -t dockerhub.suntekcorps.com:8443/suntek/$BASE_IMAGE_PROJECT:$BASE_IMAGE_VERSION --build-arg base_image_project=$BASE_IMAGE_PROJECT --build-arg  base_image_version=$BASE_IMAGE_VERSION .
-docker build  --squash -t dockerhub.suntekcorps.com:8443/suntek/$BASE_IMAGE_PROJECT:$BASE_IMAGE_VERSION --build-arg base_image_project=$BASE_IMAGE_PROJECT --build-arg  base_image_version=$BASE_IMAGE_VERSION .
+docker build --squash -t $ORIGIN_TAG --build-arg base_image_project=$BASE_IMAGE_PROJECT --build-arg  base_image_version=$BASE_IMAGE_VERSION .
 #sudo docker build  -t dockerhub.suntekcorps.com:8443/suntek/$BASE_IMAGE_PROJECT:$BASE_IMAGE_VERSION --build-arg base_image_project=$BASE_IMAGE_PROJECT --build-arg  base_image_version=$BASE_IMAGE_VERSION .
+
+# 生成　dockerfile.build 
+
+echo "FROM $ORIGIN_TAG AS source \n\
+
+FROM almalinux:8 \n\
+
+MAINTAINER sunny5156 <sunny5156@qq.com> \n\
+
+COPY --from=source / / \n\
+
+EXPOSE 22 80 443 8080 8000 \n\
+ENTRYPOINT [\"\/run.sh\"]
+
+" > ./Dockerfile.build
+
+
+# SQUASH_TAG
+docker build  -t $SQUASH_TAG --build-arg base_image_project=$BASE_IMAGE_PROJECT --build-arg  base_image_version=$BASE_IMAGE_VERSION -f ./Dockerfile.build .
